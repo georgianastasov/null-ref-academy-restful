a1b5e3c12f9c4f9fdfa5fbab54fe74f5c674d70f
Georgi Anastasov <ggeorgianastasov@gmail.com>
2023-08-01 18:08:08 +0300

    New model for news
    #7 #11
    - New model news for new functionalities

---
+++ NullRefAcademyBack/NullRefAcademy/Models/News.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace NullRefAcademy.Models
+{
+    public class News
+    {
+        [Key, Column(Order = 1)]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter Title")]
+        [StringLength(100, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please enter Description")]
+        [StringLength(500, MinimumLength = 3)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please enter Text")]
+        [StringLength(3000, MinimumLength = 3)]
+        public string Text { get; set; }
+
+        [DisplayName("Created Date")]
+        public string? CreatedDate { get; set; }
+
+        [DisplayName("Admin")]
+        public int? AdminID { get; set; }
+    }
+}
