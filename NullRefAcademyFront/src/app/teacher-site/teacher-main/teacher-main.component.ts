58a9c4ea459461c18512841188e4387ad8754a55
Georgi Anastasov <ggeorgianastasov@gmail.com>
2023-08-04 19:08:21 +0300

    Show articles of the teacher in the main teacher page
    #5 #7
    - Showing articles count that teacher has in the main dashboard of the teacher panel

--- NullRefAcademyFront/src/app/teacher-site/teacher-main/teacher-main.component.ts
+++ NullRefAcademyFront/src/app/teacher-site/teacher-main/teacher-main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { Article } from 'src/app/models/article.model';
 import { Category } from 'src/app/models/category.model';
 import { Course } from 'src/app/models/course.model';
 import { Section } from 'src/app/models/section.model';
@@ -47,6 +48,7 @@ export class TeacherMainComponent implements OnInit {
   categories: Category[] = [];
   courses: Course[] = [];
   sections: Section[] = [];
+  articles: Article[] = [];
   students: Student[] = [];
 
   ngOnInit(): void {
@@ -65,6 +67,7 @@ export class TeacherMainComponent implements OnInit {
     this.getCountCategories();
     this.getCountCourses();
     this.getCountSections();
+    this.getCountArticles();
   }
 
   ngOnDestroy() {
@@ -246,6 +249,23 @@ export class TeacherMainComponent implements OnInit {
                     });
                   }
                 );
+            }
+          });
+        }
+      );
+  }
+
+  countArticles: number = 0;
+  getCountArticles() {
+    this.service.getAllArticles()
+      .subscribe(
+        response => {
+          this.articles = response;
+          this.articles.forEach(article => {
+            if (article.teacherID != null) {
+              if (article.teacherID == this.routeid) {
+                this.countArticles++;
+              }
             }
           });
         }
