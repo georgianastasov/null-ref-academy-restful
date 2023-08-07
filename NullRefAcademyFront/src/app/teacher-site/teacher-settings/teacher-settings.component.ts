e7baeacc48228ea12a523c6c482693932c8333d9
Georgi Anastasov <ggeorgianastasov@gmail.com>
2023-08-07 16:59:42 +0300

    Updating teacher main pages with new changes
    #3 #5
    - New changes in the teacher panel about the article feature

--- NullRefAcademyFront/src/app/teacher-site/teacher-settings/teacher-settings.component.ts
+++ NullRefAcademyFront/src/app/teacher-site/teacher-settings/teacher-settings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { Article } from 'src/app/models/article.model';
 import { Category } from 'src/app/models/category.model';
 import { Course } from 'src/app/models/course.model';
 import { Section } from 'src/app/models/section.model';
@@ -45,6 +46,7 @@ export class TeacherSettingsComponent implements OnInit {
   categories: Category[] = [];
   courses: Course[] = [];
   sections: Section[] = [];
+  articles: Article[] = [];
 
   ngOnInit(): void {
     this.routeSub = this.route.params.subscribe(params => {
@@ -60,6 +62,7 @@ export class TeacherSettingsComponent implements OnInit {
     this.getCountCategories();
     this.getCountCourses();
     this.getCountSections();
+    this.getCountArticles();
   }
 
   ngOnDestroy() {
@@ -229,4 +232,20 @@ export class TeacherSettingsComponent implements OnInit {
       );
   }
 
+  countArticles: number = 0;
+  getCountArticles() {
+    this.service.getAllArticles()
+      .subscribe(
+        response => {
+          this.articles = response;
+          this.articles.forEach(article => {
+            if (article.teacherID != null) {
+              if (article.teacherID == this.routeid) {
+                this.countArticles++;
+              }
+            }
+          });
+        }
+      );
+  }
 }
