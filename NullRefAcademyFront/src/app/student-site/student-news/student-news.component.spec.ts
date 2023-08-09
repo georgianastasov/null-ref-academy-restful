8bdafba749e39f8833936a930a28dd612d648afd
Georgi Anastasov <ggeorgianastasov@gmail.com>
2023-08-09 17:55:34 +0300

    New page show news in the student panel
    #5 #7
    - New page show news for the new feature news in student panel

---
+++ NullRefAcademyFront/src/app/student-site/student-news/student-news.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StudentNewsComponent } from './student-news.component';
+
+describe('StudentNewsComponent', () => {
+  let component: StudentNewsComponent;
+  let fixture: ComponentFixture<StudentNewsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StudentNewsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StudentNewsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ NullRefAcademyFront/src/app/student-site/student-news/student-news.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Admin } from 'src/app/models/admin.model';
+import { Article } from 'src/app/models/article.model';
+import { Category } from 'src/app/models/category.model';
+import { Course } from 'src/app/models/course.model';
+import { News } from 'src/app/models/news.model';
+import { Section } from 'src/app/models/section.model';
+import { Student } from 'src/app/models/student.model';
+import { StudentCourses } from 'src/app/models/studentcourses.model';
+import { Teacher } from 'src/app/models/teacher.model';
+import { StudentApiService } from 'src/app/services/student-api.service';
+
+@Component({
+  selector: 'app-student-news',
+  templateUrl: './student-news.component.html',
+  styleUrls: ['./student-news.component.css']
+})
+export class StudentNewsComponent implements OnInit {
+
+  routeSub!: Subscription;
+  routeid!: number;
+  newsid!: number;
+  constructor(private service: StudentApiService, private router: Router, private route: ActivatedRoute) { }
+
+  student: Student = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    accountType: '',
+    password: '',
+    confirmPassword: '',
+    createdDate: '',
+    bio: '',
+    points: 0,
+    progress: 0,
+    timeSpent: '',
+    coursesIDs: ''
+  }
+  
+  admins: Admin[] = [];
+  newss: News[] = [];
+  
+  ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe(params => {
+      this.routeid = params['id'];
+      this.newsid = params['id2'];
+      console.log('courseidd:' + this.newsid)
+    });
+
+    this.getStudent();
+
+    this.getAllAdmins();
+    this.getAllNews();
+  }
+
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
+  }
+
+  getStudent() {
+    this.service.getStudent(this.routeid)
+      .subscribe(
+        response => {
+          this.student = response;
+        }
+      );
+  }
+
+  getAllAdmins() {
+    this.service.getAllAdmins()
+    .subscribe(
+      response => {
+        this.admins = response;
+      }
+    );
+  }
+
+  getAllNews() {
+    this.service.getAllNews()
+      .subscribe(
+        response => {
+          this.newss = response;
+        }
+      );
+  }
+}
