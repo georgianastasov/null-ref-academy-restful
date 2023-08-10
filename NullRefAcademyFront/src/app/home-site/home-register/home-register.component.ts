56b048124694523b677f455c00a23848cc456432
Georgi Anastasov <ggeorgianastasov@gmail.com>
2023-08-10 18:42:40 +0300

    Adding validation for the register form
    #3 #5 #8
    - Adding new validations for each field in  the register form

--- NullRefAcademyFront/src/app/home-site/home-register/home-register.component.ts
+++ NullRefAcademyFront/src/app/home-site/home-register/home-register.component.ts
@@ -1,9 +1,9 @@
 import { identifierName } from '@angular/compiler';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { last, map, Observable } from 'rxjs';
 import { HomeApiService } from '../../services/home-api.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from '../../models/user.model';
 
 @Component({
@@ -12,6 +12,43 @@ import { User } from '../../models/user.model';
   styleUrls: ['./home-register.component.css']
 })
 export class HomeRegisterComponent implements OnInit {
+  @ViewChild('select', {static:true}) public select!: ElementRef;
+  @ViewChild('passwordInput', {static:true}) public passwordInput!: ElementRef;
+  @ViewChild('confirmPasswordInput', {static:true}) public confirmPasswordInput!: ElementRef;
+  
+  public hasErrorFirstName = false;
+  public hasErrorFirstNameRequired: boolean = false;
+  public hasErrorFirstNameLength: boolean = false;
+
+  public hasErrorLastName = false;
+  public hasErrorLastNameRequired: boolean = false;
+  public hasErrorLastNameLength: boolean = false;
+
+  public hasErrorUsername = false;
+  public hasErrorUsernameRequired: boolean = false;
+  public hasErrorUsernameLength: boolean = false;
+  
+  public hasErrorEmail: boolean = false;
+  public hasErrorEmailRegex: boolean = false;
+
+  public hasErrorAccountType: boolean = false;
+
+  public hasErrorAccountTypeRepeat: boolean = false;
+  public hasErrorAccountTypeRepeatRequired: boolean = false;
+  public hasErrorAccountTypeNotSame: boolean = false;
+
+  public hasErrorPassword: boolean = false;
+  public hasErrorPasswordRequired: boolean = false;
+  public hasErrorPasswordRegex: boolean = false;
+
+  public hasErrorConfirmPassword: boolean = false; 
+  public hasErrorConfirmPasswordRequired: boolean = false;
+  public hasErrorConfirmPasswordNotSame: boolean = false;
+
+  public submited = false;
+
+  private regExpEmail = new RegExp('[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}', 'g');
+  private regExpPassword = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$', 'g');
 
   constructor(private service: HomeApiService, private router: Router) { }
   
@@ -27,54 +64,139 @@ export class HomeRegisterComponent implements OnInit {
     createdDate: ''
   }
 
-  ngOnInit(): void {
-    
-  }
-
-  hasErrorEmail: boolean = false;
-  hasErrorFirstName: boolean = false;
-  hasErrorLastName: boolean = false;
-  hasErrorUsername: boolean = false;
-  hasErrorPassword: boolean = false;
-  hasErrorConfirmPassword: boolean = false;
-  hasErrorAccountType: boolean = false;
+  ngOnInit(): void {}
   
   onSubmit() {
+    this.submited = true;
+    if (this.select.nativeElement.value === "Select account type") {
+      this.hasErrorAccountType = true;
+    } else {
+      this.hasErrorAccountType = false;
+    }
+    if (this.user.accountType != '' && this.user.accountType !== this.select.nativeElement.value) {
+      this.hasErrorAccountTypeRepeat = true;
+      this.hasErrorAccountTypeNotSame = true;
+    } else {
+      this.hasErrorAccountTypeRepeat = false;
+      this.hasErrorAccountTypeNotSame = false;
+    }
+    let matchEmail = this.regExpEmail.test(this.user.email);
+    if(!matchEmail){
+      this.hasErrorEmail = true;
+      this.hasErrorEmailRegex = true;
+    } else {
+      this.hasErrorEmail = false;
+      this.hasErrorEmailRegex = false;
+    }
+    let matchPassword = this.regExpPassword.test(this.user.password);
+    if(!matchPassword){
+      this.hasErrorPassword = true;
+      this.hasErrorPasswordRegex = true;
+    } else {
+      this.hasErrorPassword = false;
+      this.hasErrorPasswordRegex = false;
+    }
+    if (this.passwordInput.nativeElement.value !== this.confirmPasswordInput.nativeElement.value) {
+      this.hasErrorConfirmPassword = true;
+      this.hasErrorConfirmPasswordNotSame = true;
+    } else {
+      this.hasErrorConfirmPassword = false;
+      this.hasErrorConfirmPasswordNotSame = false;
+    }
     this.service.registerUser(this.user)
     .subscribe(
       response => {
         this.router.navigate(['/Home/Login']);
       },
-      err => {
-        console.log(err.error.errors)
-        this.hasErrorEmail = false;
-        this.hasErrorFirstName = false;
-        this.hasErrorLastName = false;
-        this.hasErrorUsername = false;
-        this.hasErrorPassword = false;
-        this.hasErrorConfirmPassword = false;
-        this.hasErrorAccountType = false;
-        if (err.error.errors != null) {
-          if (err.error.errors.Email != null) {
-            this.hasErrorEmail = true; 
+      error => {
+        console.log(error.error.errors)
+        let theError = error.error.errors;
+        if (theError != null) {
+          //first name
+          if (theError.FirstName != null) {
+            this.hasErrorFirstName = true;
+            if(theError.FirstName[0]){
+              this.hasErrorFirstNameRequired = true;
+            } else {
+              this.hasErrorFirstNameLength = false;
+            }
+            if(theError.FirstName[1]){
+              this.hasErrorFirstNameLength = true;
+            } else {
+              this.hasErrorFirstNameRequired = false;
+            }
+          } else {
+            this.hasErrorFirstName = false;
+            this.hasErrorFirstNameRequired = false;
+            this.hasErrorFirstNameLength = false;
           }
-          if (err.error.errors.FirstName != null) {
-            this.hasErrorFirstName = true; 
+          //last name
+          if (theError.LastName != null) {
+            this.hasErrorLastName = true;
+            if(theError.LastName[0]){
+              this.hasErrorLastNameRequired = true;
+            } else {
+              this.hasErrorLastNameLength = false;
+            }
+            if(theError.LastName[1]){
+              this.hasErrorLastNameLength = true;
+            } else {
+              this.hasErrorLastNameRequired = false;
+            }
+          } else {
+            this.hasErrorLastName = false;
+            this.hasErrorLastNameRequired = false;
+            this.hasErrorLastNameLength = false;
           }
-          if (err.error.errors.LastName != null) {
-            this.hasErrorLastName = true; 
+          //username
+          if (theError.Username != null) {
+            this.hasErrorUsername = true;
+            if(theError.LastName[0]){
+              this.hasErrorUsernameRequired = true;
+            } else {
+              this.hasErrorUsernameLength = false;
+            }
+            if(theError.LastName[1]){
+              this.hasErrorUsernameLength = true;
+            } else {
+              this.hasErrorUsernameRequired = false;
+            }
+          } else {
+            this.hasErrorUsername = false;
+            this.hasErrorUsernameRequired = false;
+            this.hasErrorUsernameLength = false;
           }
-          if (err.error.errors.Username != null) {
-            this.hasErrorUsername = true; 
+          //emai
+          if (theError.Email != null) {
+            this.hasErrorEmail = true;
+          } else {
+            this.hasErrorEmail = false;
           }
-          if (err.error.errors.Password != null) {
-            this.hasErrorPassword = true; 
+          //account type confirm
+          if (theError.AccountType != null) {
+            this.hasErrorAccountTypeRepeat = true;
+            this.hasErrorAccountTypeRepeatRequired = true;
+          } else {
+            this.hasErrorAccountTypeRepeat = false;
+            this.hasErrorAccountTypeRepeatRequired = false;
           }
-          if (err.error.errors.ConfirmPassword != null) {
-            this.hasErrorConfirmPassword = true; 
+          //password
+          if (theError.Password != null) {
+            if(!theError.Password[0].includes('regular')){
+              this.hasErrorPassword = true;
+              this.hasErrorPasswordRequired = true;
+            }
+          } else {
+            this.hasErrorPassword = false;
+            this.hasErrorPasswordRequired = false;
           }
-          if (err.error.errors.AccountType != null) {
-            this.hasErrorAccountType = true; 
+          //confirm password
+          if (theError.ConfirmPassword != null) {
+            this.hasErrorConfirmPassword = true;
+            this.hasErrorConfirmPasswordRequired = true;
+          } else {
+            this.hasErrorConfirmPassword = false;
+            this.hasErrorConfirmPasswordRequired = false;
           }
         }
       }
