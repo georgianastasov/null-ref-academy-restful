6281ba5b36154cb137c77303cbdf9131c62f6231
Georgi Anastasov <ggeorgianastasov@gmail.com>
2023-08-11 19:01:44 +0300

    Adding validation for the add categories and sections admin panel
    #6 #7 #8
    - Adding validation for the add categories and sections in the admin panel

--- NullRefAcademyFront/src/app/admin-site/admin-add-category/admin-add-category.component.ts
+++ NullRefAcademyFront/src/app/admin-site/admin-add-category/admin-add-category.component.ts
@@ -10,10 +10,20 @@ import { AdminApiService } from 'src/app/services/admin-api.service';
   styleUrls: ['./admin-add-category.component.css']
 })
 export class AdminAddCategoryComponent implements OnInit {
+  public routeSub!: Subscription;
+  public routeid!: number;
+  public nullfield!: number;
 
-  routeSub!: Subscription;
-  routeid!: number;
-  nullfield!: number;
+  public hasErrorTitle = false;
+  public hasErrorTitleRequired: boolean = false;
+  public hasErrorTitleLength: boolean = false;
+
+  public hasErrorDescription = false;
+  public hasErrorDescriptionRequired: boolean = false;
+  public hasErrorDescriptionLength: boolean = false;
+
+  public submited = false;
+
   constructor(private service: AdminApiService, private router: Router, private route: ActivatedRoute) { }
 
   category: Category = {
@@ -26,8 +36,6 @@ export class AdminAddCategoryComponent implements OnInit {
 
   ngOnInit(): void {
     this.routeSub = this.route.params.subscribe(params => {
-      console.log(params);
-      console.log('id: ' + params['id']);
       this.routeid = params['id'];
     });
   }
@@ -37,12 +45,53 @@ export class AdminAddCategoryComponent implements OnInit {
   }
 
   onSubmit() {
+    this.submited = true;
     this.category.adminID = this.routeid;
     this.service.createCategory(this.category)
     .subscribe(
       response => {
-        console.log(response);
         this.router.navigate(['/Admin/' + this.routeid + '/Categories']);
+      },
+      error => {
+        let theError = error.error.errors;
+        if (theError != null) {
+          //title
+          if (theError.Title != null) {
+            this.hasErrorTitle = true;
+            if(theError.Title[0]){
+              this.hasErrorTitleRequired = true;
+            } else {
+              this.hasErrorTitleLength = false;
+            }
+            if(theError.Title[1]){
+              this.hasErrorTitleLength = true;
+            } else {
+              this.hasErrorTitleRequired = false;
+            }
+          } else {
+            this.hasErrorTitle = false;
+            this.hasErrorTitleRequired = false;
+            this.hasErrorTitleLength = false;
+          }
+          //descriotion
+          if (theError.Description != null) {
+            this.hasErrorDescription = true;
+            if(theError.Description[0]){
+              this.hasErrorDescriptionRequired = true;
+            } else {
+              this.hasErrorDescriptionLength = false;
+            }
+            if(theError.Description[1]){
+              this.hasErrorDescriptionLength = true;
+            } else {
+              this.hasErrorDescriptionRequired = false;
+            }
+          } else {
+            this.hasErrorDescription = false;
+            this.hasErrorDescriptionRequired = false;
+            this.hasErrorDescriptionLength = false;
+          }
+        }
       }
     )
   }
--- NullRefAcademyFront/src/app/admin-site/admin-add-section/admin-add-section.component.ts
+++ NullRefAcademyFront/src/app/admin-site/admin-add-section/admin-add-section.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { error } from 'console';
 import { Subscription } from 'rxjs';
 import { Section } from 'src/app/models/section.model';
 import { AdminApiService } from 'src/app/services/admin-api.service';
@@ -10,10 +11,32 @@ import { AdminApiService } from 'src/app/services/admin-api.service';
   styleUrls: ['./admin-add-section.component.css']
 })
 export class AdminAddSectionComponent implements OnInit {
+  public routeSub!: Subscription;
+  public routeid!: number;
+  public nullfield!: number;
 
-  routeSub!: Subscription;
-  routeid!: number;
-  nullfield!: number;
+  public hasErrorTitle = false;
+  public hasErrorTitleRequired: boolean = false;
+  public hasErrorTitleLength: boolean = false;
+
+  public hasErrorDescription = false;
+  public hasErrorDescriptionRequired: boolean = false;
+  public hasErrorDescriptionLength: boolean = false;
+
+  public hasErrorText = false;
+  public hasErrorTextRequired: boolean = false;
+  public hasErrorTextLength: boolean = false;
+
+  public hasErrorVidoeUrl = false;
+  public hasErrorVidoeUrlRegex = false;
+
+  public hasErrorCourse = false;
+  public hasErrorCourseRequired: boolean = false;
+
+  public submited = false;
+
+  private regExpVideo = new RegExp('(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?', 'g');
+
   constructor(private service: AdminApiService, private router: Router, private route: ActivatedRoute) { }
 
   section: Section = {
@@ -22,7 +45,7 @@ export class AdminAddSectionComponent implements OnInit {
     description: '',
     text: '',
     videoUrl: '',
-    courseID: 0,
+    courseID: this.nullfield,
     teacherID: this.nullfield,
     adminID: 0
   }
@@ -33,8 +56,6 @@ export class AdminAddSectionComponent implements OnInit {
     this.getAllCourses();
 
     this.routeSub = this.route.params.subscribe(params => {
-      console.log(params);
-      console.log('id: ' + params['id']);
       this.routeid = params['id'];
     });
   }
@@ -44,13 +65,93 @@ export class AdminAddSectionComponent implements OnInit {
   }
 
   onSubmit() {
+    this.submited = true;
+    //vidoeurl
+    if(this.section.videoUrl != ''){
+      let matchVideo = this.regExpVideo.test(this.section.videoUrl);
+      if(!matchVideo){
+        this.hasErrorVidoeUrl = true;
+        this.hasErrorVidoeUrlRegex = true;
+      } else {
+        this.hasErrorVidoeUrl = false;
+        this.hasErrorVidoeUrlRegex = false;
+      }
+    } else {
+      this.hasErrorVidoeUrl = false;
+      this.hasErrorVidoeUrlRegex = false;
+    }
+    //course
+    if(this.section.courseID === this.nullfield){
+      this.hasErrorCourse = true;
+      this.hasErrorCourseRequired = true;
+    } else {
+      this.hasErrorCourse = false;
+      this.hasErrorCourseRequired = false;
+    }
     this.section.adminID = this.routeid;
     this.service.createSection(this.section)
     .subscribe(
       response => {
-        console.log(response);
-        console.log('Section:' + this.section);
         this.router.navigate(['/Admin/' + this.routeid + '/Sections']);
+      }, 
+      error => {
+        let theError = error.error.errors;
+        if (theError != null) {
+          //title
+          if (theError.Title != null) {
+            this.hasErrorTitle = true;
+            if(theError.Title[0]){
+              this.hasErrorTitleRequired = true;
+            } else {
+              this.hasErrorTitleLength = false;
+            }
+            if(theError.Title[1]){
+              this.hasErrorTitleLength = true;
+            } else {
+              this.hasErrorTitleRequired = false;
+            }
+          } else {
+            this.hasErrorTitle = false;
+            this.hasErrorTitleRequired = false;
+            this.hasErrorTitleLength = false;
+          }
+          //descriotion
+          if (theError.Description != null) {
+            this.hasErrorDescription = true;
+            if(theError.Description[0]){
+              this.hasErrorDescriptionRequired = true;
+            } else {
+              this.hasErrorDescriptionLength = false;
+            }
+            if(theError.Description[1]){
+              this.hasErrorDescriptionLength = true;
+            } else {
+              this.hasErrorDescriptionRequired = false;
+            }
+          } else {
+            this.hasErrorDescription = false;
+            this.hasErrorDescriptionRequired = false;
+            this.hasErrorDescriptionLength = false;
+          }
+          //text
+          if (theError.Text != null) {
+            this.hasErrorText = true;
+            if(theError.Text[0]){
+              this.hasErrorTextRequired = true;
+            } else {
+              this.hasErrorTextLength = false;
+            }
+            if(theError.Text[1]){
+              this.hasErrorTextLength = true;
+            } else {
+              this.hasErrorTextRequired = false;
+            }
+          } else {
+            this.hasErrorText = false;
+            this.hasErrorTextRequired = false;
+            this.hasErrorTextLength = false;
+          }
+        }
       }
     )
   }
@@ -59,9 +160,7 @@ export class AdminAddSectionComponent implements OnInit {
     this.service.getAllCourses()
     .subscribe(
       response => {
-        console.log('Courses');
         this.courses = response;
-        console.log(this.courses);
       }
     );
   }
