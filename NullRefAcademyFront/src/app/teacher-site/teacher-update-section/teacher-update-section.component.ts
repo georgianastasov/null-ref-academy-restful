42acabdae997f4c7f4cd70a53d6dbdc1459b4786
Georgi Anastasov <ggeorgianastasov@gmail.com>
2023-08-15 16:39:54 +0300

    Adding validation for the update sections teacher panel
    #5 #7 #8
    - Adding validation for the update sections in the teacher panel

--- NullRefAcademyFront/src/app/teacher-site/teacher-update-section/teacher-update-section.component.ts
+++ NullRefAcademyFront/src/app/teacher-site/teacher-update-section/teacher-update-section.component.ts
@@ -15,10 +15,32 @@ import { TeacherApiService } from 'src/app/services/teacher-api.service';
   styleUrls: ['./teacher-update-section.component.css']
 })
 export class TeacherUpdateSectionComponent implements OnInit {
+  public routeSub!: Subscription;
+  public routeid!: number;
+  public sectionid!: number;
 
-  routeSub!: Subscription;
-  routeid!: number;
-  sectionid!: number;
+  public hasErrorTitle = false;
+  public hasErrorTitleRequired: boolean = false;
+  public hasErrorTitleLength: boolean = false;
+
+  public hasErrorDescription = false;
+  public hasErrorDescriptionRequired: boolean = false;
+  public hasErrorDescriptionLength: boolean = false;
+
+  public hasErrorText = false;
+  public hasErrorTextRequired: boolean = false;
+  public hasErrorTextLength: boolean = false;
+
+  public hasErrorVidoeUrl = false;
+  public hasErrorVidoeUrlRegex = false;
+
+  public hasErrorCourse = false;
+  public hasErrorCourseRequired: boolean = false;
+
+  public submited = false;
+
+  private regExpVideo = new RegExp('(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?', 'g');
+
   constructor(private service: TeacherApiService, private router: Router, private route: ActivatedRoute) { }
 
   teacher: Teacher = {
@@ -87,10 +109,84 @@ export class TeacherUpdateSectionComponent implements OnInit {
 
   onSubmit() {
     this.populate();
+    this.submited = true;
+    //vidoeurl
+    if(this.section.videoUrl != ''){
+      let matchVideo = this.regExpVideo.test(this.section.videoUrl);
+      if(!matchVideo){
+        this.hasErrorVidoeUrl = true;
+        this.hasErrorVidoeUrlRegex = true;
+      } else {
+        this.hasErrorVidoeUrl = false;
+        this.hasErrorVidoeUrlRegex = false;
+      }
+    } else {
+      this.hasErrorVidoeUrl = false;
+      this.hasErrorVidoeUrlRegex = false;
+    }
     this.service.updateSection(this.sectionid, this.takenSection)
     .subscribe(
       response => {
         this.router.navigate(['/Teacher/' + this.routeid + '/Dashboard']);
+      },
+      error => {
+        let theError = error.error.errors;
+        if (theError != null) {
+          //title
+          if (theError.Title != null) {
+            this.hasErrorTitle = true;
+            if(theError.Title[0]){
+              this.hasErrorTitleRequired = true;
+            } else {
+              this.hasErrorTitleLength = false;
+            }
+            if(theError.Title[1]){
+              this.hasErrorTitleLength = true;
+            } else {
+              this.hasErrorTitleRequired = false;
+            }
+          } else {
+            this.hasErrorTitle = false;
+            this.hasErrorTitleRequired = false;
+            this.hasErrorTitleLength = false;
+          }
+          //descriotion
+          if (theError.Description != null) {
+            this.hasErrorDescription = true;
+            if(theError.Description[0]){
+              this.hasErrorDescriptionRequired = true;
+            } else {
+              this.hasErrorDescriptionLength = false;
+            }
+            if(theError.Description[1]){
+              this.hasErrorDescriptionLength = true;
+            } else {
+              this.hasErrorDescriptionRequired = false;
+            }
+          } else {
+            this.hasErrorDescription = false;
+            this.hasErrorDescriptionRequired = false;
+            this.hasErrorDescriptionLength = false;
+          }
+          //text
+          if (theError.Text != null) {
+            this.hasErrorText = true;
+            if(theError.Text[0]){
+              this.hasErrorTextRequired = true;
+            } else {
+              this.hasErrorTextLength = false;
+            }
+            if(theError.Text[1]){
+              this.hasErrorTextLength = true;
+            } else {
+              this.hasErrorTextRequired = false;
+            }
+          } else {
+            this.hasErrorText = false;
+            this.hasErrorTextRequired = false;
+            this.hasErrorTextLength = false;
+          }
+        }
       }
     )
   }
