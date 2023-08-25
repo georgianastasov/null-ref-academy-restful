0a961f0637182c4e8e3cd006ca271b54369e26ac
Georgi Anastasov <ggeorgianastasov@gmail.com>
2023-08-25 17:41:56 +0300

    Creating the migrations to the database
    #1 #9 #10
    - Creating the new migrations with the new changed models in the back end

--- NullRefAcademyBack/NullRefAcademy/Migrations/20230825142214_AddFilelds.Designer.cs
+++ NullRefAcademyBack/NullRefAcademy/Migrations/20230825142214_AddFilelds.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NullRefAcademy.Data;
 
@@ -11,9 +12,10 @@ using NullRefAcademy.Data;
 namespace NullRefAcademy.Migrations
 {
     [DbContext(typeof(ApplicationDataContext))]
-    partial class ApplicationDataContextModelSnapshot : ModelSnapshot
+    [Migration("20230825142214_AddFilelds")]
+    partial class AddFilelds
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ NullRefAcademyBack/NullRefAcademy/Migrations/20230825142214_AddFilelds.cs
@@ -0,0 +1,289 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NullRefAcademy.Migrations
+{
+    public partial class AddFilelds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ArticleIDs",
+                table: "Teachers",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NewsIDs",
+                table: "Teachers",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ArticleIDs",
+                table: "Students",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NewsIDs",
+                table: "Students",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VideoUrl",
+                table: "Sections",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RatingQty",
+                table: "News",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "News",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentsIDs",
+                table: "News",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TeacherIDs",
+                table: "News",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VideoUrl",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VideoUrl",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentsIDs",
+                table: "Courses",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RatingQty",
+                table: "Courses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "Courses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RatingQty",
+                table: "Articles",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "Articles",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentsIDs",
+                table: "Articles",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TeacherIDs",
+                table: "Articles",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VideoUrl",
+                table: "Articles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArticleIDs",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "NewsIDs",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "ArticleIDs",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "NewsIDs",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "StudentsIDs",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "TeacherIDs",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "VideoUrl",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "StudentsIDs",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "TeacherIDs",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "VideoUrl",
+                table: "Articles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VideoUrl",
+                table: "Sections",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RatingQty",
+                table: "News",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "News",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VideoUrl",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentsIDs",
+                table: "Courses",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3000)",
+                oldMaxLength: 3000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RatingQty",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RatingQty",
+                table: "Articles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "Articles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
