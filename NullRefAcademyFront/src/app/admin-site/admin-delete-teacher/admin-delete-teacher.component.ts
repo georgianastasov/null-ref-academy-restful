5ac777df70a05e4937ef5152062b2a822202acf8
Georgi Anastasov <ggeorgianastasov@gmail.com>
2023-08-30 19:31:28 +0300

    Adding new fields in the delete teacher
    #6 #7 #8
    - Adding new fields for the new properties for the delete teacher in the admin panel

--- NullRefAcademyFront/src/app/admin-site/admin-delete-teacher/admin-delete-teacher.component.ts
+++ NullRefAcademyFront/src/app/admin-site/admin-delete-teacher/admin-delete-teacher.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { Article } from 'src/app/models/article.model';
 import { Category } from 'src/app/models/category.model';
 import { Course } from 'src/app/models/course.model';
+import { News } from 'src/app/models/news.model';
 import { Section } from 'src/app/models/section.model';
 import { Teacher } from 'src/app/models/teacher.model';
 import { AdminApiService } from 'src/app/services/admin-api.service';
@@ -43,9 +45,13 @@ export class AdminDeleteTeacherComponent implements OnInit {
     });
 
     this.getTeacher();
+    this.getArticlesOfTeacher();
     this.getCategoriesOfTeacher();
     this.getCoursesOfTeacher();
     this.getSectionsOfTeacher();
+
+    this.getEnrolledArticlesOfTeacher();
+    this.getEnrolledNewsOfTeacher();
   }
   
   ngOnDestroy() {
@@ -71,6 +77,29 @@ export class AdminDeleteTeacherComponent implements OnInit {
         this.router.navigate(['/Admin/' + this.routeid + '/Teachers']);
       }
     )
+  }
+
+  //Get articles of this teacher.. 
+  articlesText: string = '';
+  articles1: Article[] = [];
+  inArticles1: boolean = false;
+
+  getArticlesOfTeacher() {
+    this.service.getAllArticles()
+    .subscribe(
+      response => {
+        this.articles1 = response;
+        this.articles1.forEach(article => { 
+          if(article.teacherID == this.teacherid){
+            this.inArticles1 = true;
+            this.articlesText += "Id:" + article.id + " " + "Title:" + article.title + "\n";
+          }
+        });
+        if(!this.inArticles1){
+          this.articlesText += "This teacher has no articles.";
+        }
+      }
+    );
   }
 
   //Get categories of this teacher.. 
@@ -144,4 +173,84 @@ export class AdminDeleteTeacherComponent implements OnInit {
       }
     );
   }
+
+  //Get enrolled articles of this teacher.. 
+  enrolledArticlesText: string = '';
+  articles: Article[] = [];
+  inArticles: boolean = false;
+  array2: string[] = [];
+  articleArray: string[] = [];
+  articleid: number = 0;
+
+  getEnrolledArticlesOfTeacher() {
+    this.service.getAllArticles()
+    .subscribe(
+      response => {
+        this.articles = response;
+        if (this.articles != null) {
+          if(!this.teacher.articleIDs){
+            this.enrolledArticlesText += "This teacher no enrolled in any articles.";
+          }
+            this.array2 = this.teacher.articleIDs.split(',');
+            this.removeNull(this.array2);
+            for (let i = 0; i < this.array2.length; i++) {
+              this.articleArray = this.array2[i].split('=');
+              this.removeNull(this.articleArray);
+              this.articleid = parseInt(this.articleArray[0]);
+              this.articles.forEach(article => {
+                if (this.articleid == article.id) {
+                  this.inArticles = true;
+                  this.enrolledArticlesText += "Id:" + article.id + " " + "Title:" + article.title + "\n";
+                }
+              });
+            }
+        }
+        if(!this.inArticles){
+          this.enrolledArticlesText += "This teacher no enrolled in any article.";
+        }
+      }
+    );
+  }
+
+  //Get enrolled news of this teacher.. 
+  enrolledNewsText: string = '';
+  news: News[] = [];
+  inNews: boolean = false;
+  array3: string[] = [];
+  newsArray: string[] = [];
+  newsid: number = 0;
+
+  getEnrolledNewsOfTeacher() {
+    this.service.getAllNews()
+    .subscribe(
+      response => {
+        this.news = response;
+        if (this.news != null) {
+          if(!this.teacher.newsIDs){
+            this.enrolledNewsText += "This teacher no enrolled in any news.";
+          }
+            this.array3 = this.teacher.newsIDs.split(',');
+            this.removeNull(this.array3);
+            for (let i = 0; i < this.array3.length; i++) {
+              this.newsArray = this.array3[i].split('=');
+              this.removeNull(this.newsArray);
+              this.newsid = parseInt(this.newsArray[0]);
+              this.news.forEach(news => {
+                if (this.newsid == news.id) {
+                  this.inNews = true;
+                  this.enrolledNewsText += "Id:" + news.id + " " + "Title:" + news.title + "\n";
+                }
+              });
+            }
+        }
+        if(!this.inNews){
+          this.enrolledNewsText += "This teacher no enrolled in any news.";
+        }
+      }
+    );
+  }
+
+  removeNull(array: string[]) {
+    return array.filter(x => x !== null)
+  };
 }
