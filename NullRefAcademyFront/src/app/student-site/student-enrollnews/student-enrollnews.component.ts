8f506d092f05c3c448ade81f15386ef997e624ef
Georgi Anastasov <ggeorgianastasov@gmail.com>
2023-09-05 19:24:26 +0300

    Adding logic for the finish reading news
    #5 #7 #8
    - Adding logic for the finishing reading news in the student panel

--- NullRefAcademyFront/src/app/student-site/student-enrollnews/student-enrollnews.component.ts
+++ NullRefAcademyFront/src/app/student-site/student-enrollnews/student-enrollnews.component.ts
@@ -77,7 +77,7 @@ export class StudentEnrollNewsComponent implements OnInit {
     this.getAllAdmins();
     this.getAllTeachers();
 
-    //this.checkFinish();
+    this.checkFinish();
   }
 
   ngOnDestroy() {
@@ -130,61 +130,61 @@ export class StudentEnrollNewsComponent implements OnInit {
   }
 
   public finish = false;
-  // checkFinish() {
-  //   this.service.getAllStudents()
-  //     .subscribe(
-  //       response => {
-  //         this.students = response;
-  //         this.students.forEach(student => {
-  //           if(student.id === Number(this.routeid)){
-  //               let coursesIDs = student.coursesIDs.split(',')
-  //               coursesIDs.pop();
-  //               for (let i = 0; i < coursesIDs.length; i++) {
-  //                 let arr = coursesIDs[i].split('=');
-  //                 let id = arr[0];
-  //                 let isFinish = arr[1];
-  //                 if(Number(id) == this.courseid){
-  //                   if(isFinish === "0"){
-  //                     this.finish = true;
-  //                     break;
-  //                   } else {
-  //                     this.finish = false;
-  //                   }
-  //                 }
-  //               }
-  //           }
-  //         });
-  //       }
-  //     );
-  // }
+  checkFinish() {
+    this.service.getAllStudents()
+      .subscribe(
+        response => {
+          this.students = response;
+          this.students.forEach(student => {
+            if(student.id === Number(this.routeid)){
+                let newsIDs = student.newsIDs.split(',')
+                newsIDs.pop();
+                for (let i = 0; i < newsIDs.length; i++) {
+                  let arr = newsIDs[i].split('=');
+                  let id = arr[0];
+                  let isFinish = arr[1];
+                  if(Number(id) == this.newsid){
+                    if(isFinish === "0"){
+                      this.finish = true;
+                      break;
+                    } else {
+                      this.finish = false;
+                    }
+                  }
+                }
+            }
+          });
+        }
+      );
+  }
 
-  // private result: string = '';
-  // finishCourse(){
-  //   this.service.getStudent(this.routeid)
-  //   .subscribe(
-  //     response => {
-  //       this.student = response;
-  //       let coursesIDs = this.student.coursesIDs.split(',')
-  //       coursesIDs.pop();
-  //       for (let i = 0; i < coursesIDs.length; i++) {
-  //         let arr = coursesIDs[i].split('=');
-  //         let id = arr[0];
-  //         let date = arr[2];
-  //         let finishDate = formatDate(Date.now(), 'dd/MM/YYYY', 'en-US').toString();
-  //         if(Number(id) == this.courseid){
-  //           this.result += id + '=' + "1" + '=' + date + '=' + finishDate;
-  //           this.student.coursesIDs = this.student.coursesIDs.replace(coursesIDs[i], this.result)
-  //           break;
-  //         }
-  //       }
-  //       this.service.updateStudent(this.routeid, this.student)
-  //       .subscribe(
-  //         response => {
-  //           setTimeout(() => {
-  //             this.router.navigate(['/Student/' + this.routeid + '/Dashboard/']);
-  //           }, 100);
-  //       })
-  //     },
-  //   );
-  // }
+  private result: string = '';
+  finishCourse(){
+    this.service.getStudent(this.routeid)
+    .subscribe(
+      response => {
+        this.student = response;
+        let newsIDs = this.student.newsIDs.split(',')
+        newsIDs.pop();
+        for (let i = 0; i < newsIDs.length; i++) {
+          let arr = newsIDs[i].split('=');
+          let id = arr[0];
+          let date = arr[2];
+          let finishDate = formatDate(Date.now(), 'dd/MM/YYYY', 'en-US').toString();
+          if(Number(id) == this.newsid){
+            this.result += id + '=' + "1" + '=' + date + '=' + finishDate;
+            this.student.newsIDs = this.student.newsIDs.replace(newsIDs[i], this.result)
+            break;
+          }
+        }
+        this.service.updateStudent(this.routeid, this.student)
+        .subscribe(
+          response => {
+            setTimeout(() => {
+              this.router.navigate(['/Student/' + this.routeid + '/Dashboard/']);
+            }, 100);
+        })
+      },
+    );
+  }
 }
