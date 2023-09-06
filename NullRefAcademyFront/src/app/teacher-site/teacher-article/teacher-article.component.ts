4444b5fdcfd8795d54207471731e7d56a98eef9f
Georgi Anastasov <ggeorgianastasov@gmail.com>
2023-09-06 17:10:58 +0300

    Adding teacher enroll article logic
    #5 #7 #8
    - Adding enrolling in article for the teachers

--- NullRefAcademyFront/src/app/teacher-site/teacher-article/teacher-article.component.ts
+++ NullRefAcademyFront/src/app/teacher-site/teacher-article/teacher-article.component.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
@@ -8,6 +9,7 @@ import { Course } from 'src/app/models/course.model';
 import { Section } from 'src/app/models/section.model';
 import { Teacher } from 'src/app/models/teacher.model';
 import { StudentApiService } from 'src/app/services/student-api.service';
+import { TeacherApiService } from 'src/app/services/teacher-api.service';
 
 @Component({
   selector: 'app-teacher-article',
@@ -19,7 +21,38 @@ export class TeacherArticleComponent implements OnInit {
   routeSub!: Subscription;
   routeid!: number;
   articleid!: number;
-  constructor(private service: StudentApiService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private service: TeacherApiService, private router: Router, private route: ActivatedRoute) { }
+
+  teacher: Teacher = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    accountType: '',
+    password: '',
+    confirmPassword: '',
+    createdDate: '',
+    articleIDs: '',
+    newsIDs: ''
+  }
+
+  article: Article = {
+    id: 0,
+    title: '',
+    description: '',
+    text: '',
+    rating: 0,
+    ratingQty: 0,
+    videoUrl: '',
+    createdDate: '',
+    teacherID: 0,
+    adminID: 0,
+    studentsIDs: '',
+    teachersIDs: '',
+    usersStudentsRateIDs: '',
+    usersTeachersRateIDs: ''
+  }
 
   admins: Admin[] = [];
   teachers: Teacher[] = [];
@@ -41,6 +74,8 @@ export class TeacherArticleComponent implements OnInit {
     this.getAllCourses();
     this.getAllSections();
     this.getAllArticles();
+
+    this.checkEnrollPreview();
   }
 
   ngOnDestroy() {
@@ -99,5 +134,58 @@ export class TeacherArticleComponent implements OnInit {
           this.articles = response;
         }
       );
+  }
+
+  public enroll = true;
+  public preview = false;
+  checkEnrollPreview() {
+    this.service.getAllArticles()
+      .subscribe(
+        response => {
+          this.articles = response;
+          this.articles.forEach(article => {
+            if(article.id === Number(this.articleid)){
+                let teacherIDs = article.teachersIDs.split(',');
+                teacherIDs.pop();
+                teacherIDs.forEach(teacherID => {
+                  if(Number(teacherID) == this.routeid){
+                    this.preview = true;
+                    this.enroll = false;
+                  }
+                });
+            }
+          });
+        }
+      );
+  }
+
+  enrollTeacher(){
+    this.service.getTeacher(this.routeid)
+    .subscribe(
+      response => {
+        this.teacher = response;
+        var date = formatDate(Date.now(), 'dd/MM/YYYY', 'en-US').toString();
+        this.teacher.articleIDs += this.articleid + '=' + '0' + '=' + date + ',';
+        this.service.updateTeacher(this.routeid, this.teacher)
+        .subscribe(
+          response => {
+        })
+
+        this.service.getArticle(this.articleid)
+        .subscribe(
+          response => {
+            this.article = response;
+            this.article.teachersIDs += this.routeid + ',';
+
+            this.service.updateArticle(this.articleid, this.article)
+              .subscribe(
+                response => {
+                  setTimeout(() => {
+                    this.router.navigate(['/Teacher/' + this.routeid + '/Article/' + this.articleid + '/Enroll']);
+                  }, 100);
+              })
+          })
+      },
+    );
   }
 }
