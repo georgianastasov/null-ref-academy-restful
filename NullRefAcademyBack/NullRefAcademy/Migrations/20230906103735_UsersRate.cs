f2624ff580a8d155619f04a1cd14f4f43ef39c5d
Georgi Anastasov <ggeorgianastasov@gmail.com>
2023-09-07 21:02:34 +0300

    Adding new migration to the database
    #1 #10
    - Adding new migration to the database from the models

---
+++ NullRefAcademyBack/NullRefAcademy/Migrations/20230906103735_UsersRate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NullRefAcademy.Migrations
+{
+    public partial class UsersRate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UsersRateIDs",
+                table: "News",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UsersRateIDs",
+                table: "Courses",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UsersRateIDs",
+                table: "Articles",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsersRateIDs",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "UsersRateIDs",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "UsersRateIDs",
+                table: "Articles");
+        }
+    }
+}
