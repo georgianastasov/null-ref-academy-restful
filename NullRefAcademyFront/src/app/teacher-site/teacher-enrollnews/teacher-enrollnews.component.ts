8f76825553e7482e5a261aaeec9f2d0855a1b679
Georgi Anastasov <ggeorgianastasov@gmail.com>
2023-09-19 17:19:29 +0300

    Adding enrolling news teacher panel
    #5 #7 #8
    - Adding enrolling in the news for the teachers teacher panel

--- NullRefAcademyFront/src/app/teacher-site/teacher-enrollnews/teacher-enrollnews.component.ts
+++ NullRefAcademyFront/src/app/teacher-site/teacher-enrollnews/teacher-enrollnews.component.ts
@@ -12,6 +12,7 @@ import { Student } from 'src/app/models/student.model';
 import { StudentCourses } from 'src/app/models/studentcourses.model';
 import { Teacher } from 'src/app/models/teacher.model';
 import { StudentApiService } from 'src/app/services/student-api.service';
+import { TeacherApiService } from 'src/app/services/teacher-api.service';
 
 @Component({
   selector: 'app-teacher-enrollnews',
@@ -23,7 +24,7 @@ export class TeacherEnrollNewsComponent implements OnInit {
   routeSub!: Subscription;
   routeid!: number;
   newsid!: number;
-  constructor(private service: StudentApiService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private service: TeacherApiService, private router: Router, private route: ActivatedRoute) { }
 
   teacher: Teacher = {
     id: 0,
@@ -50,7 +51,9 @@ export class TeacherEnrollNewsComponent implements OnInit {
     adminID: 0,
     videoUrl: '',
     studentsIDs: '',
-    teachersIDs: ''
+    teachersIDs: '',
+    usersStudentsRateIDs: '',
+    usersTeachersRateIDs: ''
   }
   
   admins: Admin[] = [];
@@ -72,7 +75,8 @@ export class TeacherEnrollNewsComponent implements OnInit {
     this.getAllAdmins();
     this.getAllTeachers();
 
-    //this.checkFinish();
+    this.checkFinish();
+    this.checkRated();
   }
 
   ngOnDestroy() {
@@ -125,61 +129,110 @@ export class TeacherEnrollNewsComponent implements OnInit {
   }
 
   public finish = false;
-  // checkFinish() {
-  //   this.service.getAllStudents()
-  //     .subscribe(
-  //       response => {
-  //         this.students = response;
-  //         this.students.forEach(student => {
-  //           if(student.id === Number(this.routeid)){
-  //               let coursesIDs = student.coursesIDs.split(',')
-  //               coursesIDs.pop();
-  //               for (let i = 0; i < coursesIDs.length; i++) {
-  //                 let arr = coursesIDs[i].split('=');
-  //                 let id = arr[0];
-  //                 let isFinish = arr[1];
-  //                 if(Number(id) == this.courseid){
-  //                   if(isFinish === "0"){
-  //                     this.finish = true;
-  //                     break;
-  //                   } else {
-  //                     this.finish = false;
-  //                   }
-  //                 }
-  //               }
-  //           }
-  //         });
-  //       }
-  //     );
-  // }
+  checkFinish() {
+    this.service.getAllTeachers()
+      .subscribe(
+        response => {
+          this.teachers = response;
+          this.teachers.forEach(teacher => {
+            if(teacher.id === Number(this.routeid)){
+                let newsIDs = teacher.newsIDs.split(',')
+                newsIDs.pop();
+                for (let i = 0; i < newsIDs.length; i++) {
+                  let arr = newsIDs[i].split('=');
+                  let id = arr[0];
+                  let isFinish = arr[1];
+                  if(Number(id) == this.newsid){
+                    if(isFinish === "0"){
+                      this.finish = true;
+                      break;
+                    } else {
+                      this.finish = false;
+                    }
+                  }
+                }
+            }
+          });
+        }
+      );
+  }
 
-  // private result: string = '';
-  // finishCourse(){
-  //   this.service.getStudent(this.routeid)
-  //   .subscribe(
-  //     response => {
-  //       this.student = response;
-  //       let coursesIDs = this.student.coursesIDs.split(',')
-  //       coursesIDs.pop();
-  //       for (let i = 0; i < coursesIDs.length; i++) {
-  //         let arr = coursesIDs[i].split('=');
-  //         let id = arr[0];
-  //         let date = arr[2];
-  //         let finishDate = formatDate(Date.now(), 'dd/MM/YYYY', 'en-US').toString();
-  //         if(Number(id) == this.courseid){
-  //           this.result += id + '=' + "1" + '=' + date + '=' + finishDate;
-  //           this.student.coursesIDs = this.student.coursesIDs.replace(coursesIDs[i], this.result)
-  //           break;
-  //         }
-  //       }
-  //       this.service.updateStudent(this.routeid, this.student)
-  //       .subscribe(
-  //         response => {
-  //           setTimeout(() => {
-  //             this.router.navigate(['/Student/' + this.routeid + '/Dashboard/']);
-  //           }, 100);
-  //       })
-  //     },
-  //   );
-  // }
+  private result: string = '';
+  finishCourse(){
+    this.service.getTeacher(this.routeid)
+    .subscribe(
+      response => {
+        this.teacher = response;
+        let newsIDs = this.teacher.newsIDs.split(',')
+        newsIDs.pop();
+        for (let i = 0; i < newsIDs.length; i++) {
+          let arr = newsIDs[i].split('=');
+          let id = arr[0];
+          let date = arr[2];
+          let finishDate = formatDate(Date.now(), 'dd/MM/YYYY', 'en-US').toString();
+          if(Number(id) == this.newsid){
+            this.result += id + '=' + "1" + '=' + date + '=' + finishDate;
+            this.teacher.newsIDs = this.teacher.newsIDs.replace(newsIDs[i], this.result)
+            break;
+          }
+        }
+        this.service.updateTeacher(this.routeid, this.teacher)
+        .subscribe(
+          response => {
+            setTimeout(() => {
+              this.router.navigate(['/Teacher/' + this.routeid + '/Dashboard/']);
+            }, 100);
+        })
+      },
+    );
+  }
+
+  countStars(news: any){
+    let arr = [];
+    let number = news.rating / news.ratingQty;
+    for (let i = 0; i < number; i++) {
+      arr[i] = i;
+    }
+    return arr;
+  }
+
+  public rated = false;
+  checkRated() {
+    this.service.getAllStudents()
+      .subscribe(
+        response => {
+          this.students = response;
+          this.students.forEach(student => {
+            if(student.id === Number(this.routeid)){
+                if(this.news.usersTeachersRateIDs){
+                  let usersRateIDs = this.news.usersTeachersRateIDs.split(',');
+                  usersRateIDs.pop();
+                  for (let i = 0; i < usersRateIDs.length; i++) {
+                    let id = usersRateIDs[0];
+                    if(Number(id) == student.id){
+                      this.rated = true;
+                      break;
+                    } else {
+                      this.rated = false;
+                    }
+                  }
+                }
+            }
+          });
+        }
+      );
+  }
+
+  rateNews(value: any){
+    this.news.rating += Number(value);
+    this.news.ratingQty += 1;
+    this.news.usersTeachersRateIDs += this.routeid + ',';
+    this.service.updateNews(this.newsid, this.news)
+        .subscribe(
+          response => {
+            setTimeout(() => {
+              this.rated = true;
+            }, 100);
+        })
+  }
 }
